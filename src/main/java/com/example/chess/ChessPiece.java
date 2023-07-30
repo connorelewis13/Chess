1c6de347f1de94fc5b8fe0a3aecdcc40577b5b30
Connor Lewis <jve9ur@virginia.edu>
2023-07-30 18:13:20 -0400

    Made abstract ChessPiece Class. Going to make individual piece classes now and implement the move() and takePiece() functions

--- src/main/java/com/example/chess/ChessPiece.java
+++ src/main/java/com/example/chess/ChessPiece.java
@@ -3,5 +3,34 @@ package com.example.chess;
 public abstract class ChessPiece {
     private int xLocation;
     private int yLocation;
+    private PieceType pieceType;
+    private PieceColor pieceColor;
+    public int getXLocation() {
+        return xLocation;
+    }
+    public void setXLocation(int xLocation) {
+        this.xLocation = xLocation;
+    }
+    public int getYLocation() {
+        return yLocation;
+    }
+    public void setYLocation(int yLocation) {
+        this.yLocation = yLocation;
+    }
+    public PieceType getPieceType() {
+        return pieceType;
+    }
+    public void setPieceType(PieceType pieceType) {
+        this.pieceType = pieceType;
+    }
 
+    public PieceColor getPieceColor() {
+        return pieceColor;
+    }
+    public void setPieceColor(PieceColor pieceColor) {
+        this.pieceColor = pieceColor;
+    }
+
+    public abstract void move();
+    public abstract void takePiece();
 }
---
+++ src/main/java/com/example/chess/PieceColor.java
@@ -0,0 +1,6 @@
+package com.example.chess;
+
+public enum PieceColor {
+    WHITE,
+    BLACK
+}
