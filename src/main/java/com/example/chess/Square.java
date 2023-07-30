bd7df6b14f678922d58076d8753d3cd763cb763f
Connor Lewis <jve9ur@virginia.edu>
2023-07-30 18:40:57 -0400

    made Square and Board classes

---
+++ src/main/java/com/example/chess/Square.java
@@ -0,0 +1,13 @@
+package com.example.chess;
+
+public class Square {
+    private ChessPiece sqaurePiece;
+
+    public ChessPiece getSqaurePiece() {
+        return sqaurePiece;
+    }
+
+    public void setSqaurePiece(ChessPiece sqaurePiece) {
+        this.sqaurePiece = sqaurePiece;
+    }
+}
