e5cc05f735465b660d4e5fd67bdaa8267b045597
Connor Lewis <jve9ur@virginia.edu>
2023-07-30 17:30:26 -0400

    adding

--- src/main/java/com/example/chess/HelloController.java
+++ src/main/java/com/example/chess/HelloController.java
@@ -6,7 +6,6 @@ import javafx.scene.control.Label;
 public class HelloController {
     @FXML
     private Label welcomeText;
-    private Label gitCheck;
 
     @FXML
     protected void onHelloButtonClick() {
