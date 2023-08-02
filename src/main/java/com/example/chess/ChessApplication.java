adb50a6f1bb09b7fd92fdabb554cdfe743045b2b
Connor Lewis <jve9ur@virginia.edu>
2023-08-02 18:13:50 -0400

    added a gridpane to the view; starting function to have board pieces show up on the buttons

--- src/main/java/com/example/chess/ChessApplication.java
+++ src/main/java/com/example/chess/ChessApplication.java
@@ -8,6 +8,8 @@ import javafx.stage.Stage;
 import java.io.IOException;
 
 public class ChessApplication extends Application {
+
+    public static Board chessBoard = new Board();
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(ChessApplication.class.getResource("hello-view.fxml"));
@@ -18,10 +20,6 @@ public class ChessApplication extends Application {
     }
 
     public static void main(String[] args) {
-        Board testBoard = new Board();
-        System.out.println(testBoard);
-        testBoard.resetBoard();
-        System.out.println(testBoard);
         launch();
     }
 }
--- src/main/java/com/example/chess/ChessController.java
+++ src/main/java/com/example/chess/ChessController.java
@@ -1,14 +1,33 @@
 package com.example.chess;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.layout.GridPane;
 
 public class ChessController {
-    @FXML
-    private Label welcomeText;
-
+    @FXML private Label welcomeText;
+    @FXML private GridPane BoardGridPane;
+    @FXML private Button StartButton;
     @FXML
     protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+
+    }
+
+    public void StartButtonPressed(ActionEvent actionEvent) {
+        ChessApplication.chessBoard.resetBoard();
+        for(int x = 0;x<8;x++){
+            for(int y=0;y<8;y++){
+                Button button = new Button();
+                button.setText(getPieceOnSquare(x,y));
+                BoardGridPane.add(button,x,y);
+            }
+        }
+    }
+
+    private String getPieceOnSquare(int x, int y) {
+        Coordinates coordinates = new Coordinates(x+1,y+1);
+        return "";
     }
 }
