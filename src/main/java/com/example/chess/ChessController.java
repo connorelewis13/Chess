0b4020110b26250ff063c576764afc1595f70b57
Connor Lewis <jve9ur@virginia.edu>
2023-08-07 19:08:36 -0400

    fixed changing of display of whose turn it is

--- src/main/java/com/example/chess/ChessController.java
+++ src/main/java/com/example/chess/ChessController.java
@@ -72,7 +72,7 @@ public class ChessController {
                     putButtonWithPiece(initialY, initialX);
                     putButtonWithPiece(finalY, finalX);
                     initialLocation = null;
-                    if(ChessApplication.chessBoard.getWhitesTurn()){
+                    if(!ChessApplication.chessBoard.getWhitesTurn()){
                         WhitesTurnLabel.setBackground(greyBG);
                         BlacksTurnLabel.setBackground(greenBG);
                     }
