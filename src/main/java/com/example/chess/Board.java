eba375a5d71e396dad1231382a1d3f73e9c8a520
Connor Lewis <jve9ur@virginia.edu>
2023-08-08 15:37:37 -0400

    set up switch/case for checking if the piece's move is valid

--- src/main/java/com/example/chess/Board.java
+++ src/main/java/com/example/chess/Board.java
@@ -74,6 +74,7 @@ public class Board {
 
     public void movePiece(Coordinates initialCor, Coordinates finalCor){
         ChessPiece piece = boardMap.get(initialCor).getSqaurePiece();
+        if(!isValidMove(initialCor,finalCor)) throw new IllegalArgumentException();
         if(whitesTurn && piece.getPieceColor()==PieceColor.WHITE){
             boardMap.put(initialCor,new Square(null));
             boardMap.put(finalCor,new Square(piece));
@@ -87,5 +88,54 @@ public class Board {
         else{
             throw new IllegalArgumentException();
         }
+    }
+
+    private boolean isValidMove(Coordinates initialCor, Coordinates finalCor) {
+        ChessPiece piece = boardMap.get(initialCor).getSqaurePiece();
+        if(piecesAreSameColor(initialCor,finalCor)) return false;
+        switch(piece.getPieceType()){
+            case PAWN:
+                return isValidPawnMove(initialCor,finalCor);
+            case ROOK:
+                return isValidRookMove(initialCor,finalCor);
+            case QUEEN:
+                return isValidQueenMove(initialCor,finalCor);
+            case KNIGHT:
+                return isValidKnightMove(initialCor,finalCor);
+            case BISHOP:
+                return isValidBishopMove(initialCor,finalCor);
+            case KING:
+                return isValidKingMove(initialCor,finalCor);
+        }
+        return false;
+    }
+
+    private boolean isValidKingMove(Coordinates initialCor, Coordinates finalCor) {
+        return false;
+    }
+
+    private boolean isValidBishopMove(Coordinates initialCor, Coordinates finalCor) {
+        return false;
+    }
+
+    private boolean isValidKnightMove(Coordinates initialCor, Coordinates finalCor) {
+        return false;
+    }
+
+    private boolean isValidQueenMove(Coordinates initialCor, Coordinates finalCor) {
+        return false;
+    }
+
+    private boolean isValidRookMove(Coordinates initialCor, Coordinates finalCor) {
+        return false;
+    }
+
+    private boolean isValidPawnMove(Coordinates initialCor, Coordinates finalCor) {
+        return false;
+    }
+
+    private boolean piecesAreSameColor(Coordinates initialCor, Coordinates finalCor) {
+        if(boardMap.get(initialCor).getSqaurePiece().getPieceColor()==boardMap.get(finalCor).getSqaurePiece().getPieceColor()) return true;
+        else return false;
     }
 }
